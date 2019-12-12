fda4b64c6dd6fc506490405acad980ef9e73f7f7
Seongjae Hwang <s.hwang@dxmcorp.com>
2019-12-12 17:22:27 +0900

    fixed pm2 function binding

--- libs/pm2wrapper.js
+++ libs/pm2wrapper.js
@@ -4,5 +4,6 @@ const pm2 = require('pm2')
 const callbackFunctions = ['connect', 'start', 'stop', 'restart', 'delete', 'reload', 'killDaemon', 'describe', 'list', 'dump', 'disconnect']
 
 module.exports = Object.fromEntries(
-  callbackFunctions.filter(fn => typeof pm2[fn] === 'function').map(fn => [fn, promisify(pm2[fn])])
+  // beware 'this'
+  callbackFunctions.filter(fn => typeof pm2[fn] === 'function').map(fn => [fn, promisify((...args) => pm2[fn](...args))])
 )
